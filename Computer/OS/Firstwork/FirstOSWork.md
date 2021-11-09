e248ab52d1dfd74c288b6a1543b111174e7c01e1
liukanglai <2821756507@qq.com>
2021-11-09 18:55:48 +0800

    os work1

--- Computer/OS/Firstwork/FirstOSWork.md
+++ Computer/OS/Firstwork/FirstOSWork.md
@@ -4,14 +4,34 @@
 
 ![pwd](pwd.png)
 
+- 善用搜索，改 $(PWD) 为 $(shell pwd) 即可:
+- `https://stackoverflow.com/questions/39107811/no-rule-to-make-target-arch-x86-entry-syscalls-syscall-32-tbl-needed-by-arch`
+- 环境变量中无 PWD
+
+- module 的加载：
 ![insmod](1in.png)
 ![rmmod](1rm.png)
 
 # 2. Kernel Data Structures
 
+- 做这个时感觉很模糊，网上看了一篇文章，恍然大悟:
+- `https://www.cnblogs.com/yangguang-it/p/11667772.html`
+- 记住函数中的那些结构体指针都只是一个 “记号”，在函数中都会具体赋值。
+- 认识 offsetof 与 container_of 宏的伟大之处
+- 简单的总结：`https://github.com/liukanglai/Learing/blob/master/Computer/OS/list.md`
+- 最好的办法还是看 list.h 的原代码，水平还不够...
+
 ![list](2in.png)
 
 # 3. UNIX Shell and History Feature
+
+- 程序还是不复杂，四个函数：输入，执行，存历史，输出历史。
+- 具体实现真麻烦啊，好多细节都要 debug，但好歹也能 debug，像上面那个就只能反复试错，有时还不得不重启，
+- 关于读入的函数，参考 `http://www.csl.mtu.edu/cs4411.ck/www/NOTES/process/fork/shell.c`
+- 写的真的好，不过改了 gets() 为 fgets()，一个回车引出好多问题，果然细节重要
+- 加了后台 '&' 的实现，注意到对 args 赋 NULL，才会结束执行，而不是内容为空
+- 存历史的时候开了10个数组，注意满和未满的情况，还有开始与结束的下标处理
+- 写代码时遇到好多问题，现在回过头来看，也没多少可写，果然还是得“跳出来”
 
 ![shell](shell.png)
 
---
+++ Computer/OS/Firstwork/simple.c
@@ -0,0 +1,104 @@
+/**
+ * simple.c
+ *
+ * A simple kernel module.
+ *
+ * To compile, run makefile by entering "make"
+ *
+ * Operating System Concepts - 10th Edition
+ * Copyright John Wiley & Sons - 2018
+ */
+
+// A good reference: https://www.cnblogs.com/yangguang-it/p/11667772.html
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+#include <linux/list.h>  // link list
+#include <linux/slab.h>  // for kmalloc
+#include <linux/types.h> // list_head
+
+struct birthday {
+  int day;
+  int month;
+  int year;
+  struct list_head list;
+};
+
+static LIST_HEAD(birthday_list);
+
+/*
+ * will create a list_head, in it: prev->next, next->prev.
+ * same as struct birthday person, than INIT_LIST_HEAD(&person.list),
+ * you can see, it's more easy.
+ */
+
+struct birthday *person; // can't put in simple_init, why?
+
+struct birthday *assign_birthday(struct birthday *person, int day, int month,
+                                 int year) {
+  person = kmalloc(sizeof(*person),
+                   GFP_KERNEL); // 这出错好久，注意不能返回空，赋值的这个 person
+                                // 这是个副本；若需，可在外部 malloc,
+                                // 可要定义多个，只能在内
+  person->day = day;
+  person->month = month;
+  person->year = year;
+  return person;
+}
+
+/* This function is called when the module is loaded. */
+int simple_init(void) {
+  printk(KERN_INFO "Loading Module\n");
+
+  int i = -1;
+  while (++i < 5) {
+    person = assign_birthday(person, 2 + i, 8 + i, 1995 + i);
+    list_add_tail(&person->list,
+                  &birthday_list); // to insert in the end, and conversely
+                                   // list_add() is in head.
+                                   // }
+  }
+  /*
+  person = assign_birthday(person, 2, 8, 1995);
+  person = kmalloc(sizeof(*person), GFP_KERNEL);
+  person->day = 2;
+  person->month = 8;
+  person->year = 1995;
+  INIT_LIST_HEAD(&person->list); // is redundance?
+  */
+
+  list_for_each_entry(
+      person, &birthday_list,
+      list) { // this person will assign in list_entry, so no need to define,
+              // just a signal. 他怎么认识 list 啊，I define it in struct?
+    printk(KERN_INFO "day: %d, month: %d, year: %d\n", person->day,
+           person->month, person->year);
+  }
+
+  return 0;
+}
+
+/* This function is called when the module is removed. */
+void simple_exit(void) {
+  struct birthday *ptr;
+  struct birthday *next;
+
+  list_for_each_entry_safe(
+      ptr, next, &birthday_list,
+      list) { // as same, the ptr, next will assign inner...
+    list_del(&ptr->list);
+    kfree(ptr);
+  }
+
+  printk(KERN_INFO "Removing Module\n");
+}
+
+/* Macros for registering module entry and exit points. */
+module_init(simple_init);
+module_exit(simple_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simple Module");
+MODULE_AUTHOR("SGG");
