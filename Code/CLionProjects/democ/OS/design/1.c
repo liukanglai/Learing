200a577d8062ead635e6ed1281995faf84e49bd8
liukanglai <2821756507@qq.com>
2021-11-19 21:35:31 +0800

    code

--- Code/CLionProjects/democ/OS/design/1.c
+++ Code/CLionProjects/democ/OS/design/1.c
@@ -1,5 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+#define j 1
 
-int main(void){
-  int
+int main(void) {
+  int a;
+  for (int i; i < 10;) {
+    int a;
+  }
+
+  printf("%d\n", a);
+  return 0;
 }
