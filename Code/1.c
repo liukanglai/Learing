a3ddcdb09f89747efeabcaaf230be5ce1c9af929
liukanglai <2821756507@qq.com>
2021-11-28 14:08:14 +0800

    ccls

--- Code/1.c
+++
@@ -1,9 +0,0 @@
-#inclu
-int main(void) {
-  int wm;
-  char a[10] = 1;
-
-  printf("%s\n%d", a, wm);
-  printf(" %d");
-  return 0;
-}
---
+++ Code/C/.ccls-cache/@home@liukanglai@Learing@Code@C/1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void) {
+  int wm;
+
+  printf("%s\n%d", a, wm);
+  printf(" %d");
+  return 0;
+}
