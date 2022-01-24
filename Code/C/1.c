b4228b59b7c8e88adb4bff792c99d48c925d128f
liukanglai <2821756507@qq.com>
2022-01-24 12:15:24 +0800

    **(a+1)

--- Code/C/1.c
+++ Code/C/1.c
@@ -1,9 +1,9 @@
 // Time:2021-12-02 15:03:08
-#include <pthread.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
-  int a;
-  printf("%d", a);
+  int a[2][2][2] = {1, 2, 3, 4, 5, 6, 7, 8};
+  printf("%d", ***(a + 1));
   return 0;
 }
