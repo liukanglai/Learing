b27548a330cefa29ab5c497529c3c537df10e9f2
liukanglai <2821756507@qq.com>
2023-02-17 21:02:37 +0800

    web study

--- Code/web-projects/test-site/scripts/main.js
+++ Code/web-projects/test-site/scripts/main.js
@@ -1,10 +1,15 @@
-let myHeading = document.querySelector("h1");
+// let/var: 一个变量
+let myHeading = document.querySelector("h1"); // 获取h1标题
 let myButton = document.querySelector("button");
 
-myHeading.textContent = "Hello world!";
+myHeading.textContent = "Hello world!"; // 设置h1标题
 document.querySelector("html").onclick = function () {
   alert("别戳我，我怕疼。");
 };
+/* document.querySelector("html").addEventListener("click", function () { // 匿名函数
+    alert("别戳我，我怕疼。");
+ });
+*/
 
 let myImage = document.querySelector("img");
 
@@ -19,10 +24,10 @@ myImage.onclick = function () {
 
 function setUserName() {
   let myName = prompt("请输入你的名字。");
-  if (!myName || myName === null) {
+  if (!myName || myName === null) { // 如果不输入，直接按确认，是空?(没有值); 而直接按取消，是null
     setUserName();
   } else {
-    localStorage.setItem("name", myName);
+    localStorage.setItem("name", myName); // localStorage API可以将数据存储在浏览器中供后续获取
     myHeading.innerHTML = "Mozilla 酷毙了，" + myName;
   }
 }
